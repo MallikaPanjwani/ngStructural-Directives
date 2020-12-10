58e8efc7b6a0626fd732f7e1cc837c3d098e6bc2
MallikaPanjwani <malli.panj@gmail.com>
2020-12-10 18:12:50 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = "Directives";
+  selectedChoice = "";
+  colors: any[] = ["red", "blue", "green", "yellow", "pink", "orange"];
+
+  choice(selectedColor: any): any {
+    this.selectedChoice = selectedColor.target.value;
+    if (this.selectedChoice === "Select Color") {
+      this.selectedChoice = "white";
+    }
+  }
 }
